5f5626ec53bbe37b61a9ab254b05c458d5425f91
Jack Mocherman <mochermanjack@gmail.com>
2016-10-13 18:39:22 -0500

    added span with class that displays green if status is connected

--- components/parts/Header.js
+++ components/parts/Header.js
@@ -14,8 +14,13 @@ const Header = React.createClass({
 
 	render() {
 		return(
-			<header>
-				<h1>{this.props.title}</h1>
+			<header className="row">
+				<div className="col-xs-10">
+					<h1>{this.props.title}</h1>
+				</div>
+				<div className="col-xs-2">
+					<span id="connection-status" className={this.props.status}></span>
+				</div>
 			</header>
 		)
 	}
