a97e5cc39cb16d7a5e20ca375f89e3e7f4ea5657
Jack Mocherman <mochermanjack@gmail.com>
2016-10-13 15:52:56 -0500

    added socket to client side in component will mount function and console logged socket id

--- components/app.js
+++ components/app.js
@@ -1,6 +1,17 @@
 const React = require('react')
+const io = require('socket.io-client')
 
 const App = React.createClass({
+
+	componentWillMount() {
+	  this.socket = io()
+	  this.socket.on('connect', this.connect)
+	},
+
+	connect() {
+		console.log(`Connected on ${this.socket.id}`)
+	},
+
 	render () {
 		return(
 			<h1>Hello World</h1>
