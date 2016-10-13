1ad99be54306a3a5bd633980318b55de89f06b06
Jack Mocherman <mochermanjack@gmail.com>
2016-10-13 13:03:28 -0500

    built basic output for app-client file

---
+++ app-client.js
@@ -0,0 +1,5 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+const App = require('./components/app.js')
+
+ReactDOM.render(<App />, document.getElementById('react-container'))
