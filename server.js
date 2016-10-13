71e9060a4f836272efc5db50734cc3e868a60da0
Jack Mocherman <mochermanjack@gmail.com>
2016-10-13 18:52:35 -0500

    added connections array with push and slice methods on socket connection/disconnection, added disconnected logic

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const connections = []
+
 app.set('port', port)
 app.use(express.static('./public'))
 
@@ -12,5 +14,13 @@ const server = app.listen(port, () => {
 const io = require('socket.io').listen(server)
 
 io.on('connection', (socket) => {
-	console.log(`Listening on ${socket.id}`)
+	connections.push(socket)
+	console.log(`Listening on ${socket.id}, ${connections.length} connections remaining`)
+
+	socket.once('disconnect', () => {
+		connections.splice(connections.indexOf(socket), 1)
+		socket.disconnect()
+		console.log(`Socket ${socket.id} disconnected ${connections.length} connections remaining`)
+	})
+
 })
